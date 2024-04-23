0d698e241f390ec4a0407bbbcc245023922f660e
Nilesh Deore <nileshdeore2623@gmail.com>
2024-04-23 20:54:37 +0530

    1.5 hr

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='bg-yellow-400 flex'>
+    <div className='bg-yellow-500 flex'>
         <Link href={'/'}>Navbar</Link>
 
         <div className='mx-2'>
